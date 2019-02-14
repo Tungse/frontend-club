cbd07cc4923a9a78f0c1e5cc52da420f0125c67f
Tungse <t.pham@stroeermediabrands.de>
2019-02-14 12:21:54 +0100

    initial commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,11 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+import * as ears from './custom-elements/ears';
+import * as eyes from './custom-elements/eyes';
+import * as mouth from './custom-elements/mouth';
+import * as nose from './custom-elements/nose';
+
+import * as shadowEars from './shadow-dom/ears';
+import * as shadowEyes from './shadow-dom/eyes';
+import * as shadowMouth from './shadow-dom/mouth';
+import * as shadowNose from './shadow-dom/nose';
