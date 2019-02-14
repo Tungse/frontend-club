ffa25b6ec678128666c28bb53951fcc0193d2fc4
aim2k1 <info@zundel-webdesign.de>
2019-02-14 15:14:00 +0100

    asdf

--- src/scripts/custom-elements/nose.js
+++ src/scripts/custom-elements/nose.js
@@ -1,18 +1,14 @@
-// if ("customElements" in window) {
-  class SmbNose extends HTMLElement {
-    constructor() {
-      super();
-      let i = 333333;
-      setInterval(() => {
-        // i++;
-        // this.style.backgroundColor = '#' + i;
-        this.className = 'purple';
-        setTimeout(() => {
-          this.className = '';
-         }, 3000);
-     }, 2000);
-    }
+class SmbNose extends HTMLElement {
+  constructor() {
+    super();
+    let i = 333333;
+    setInterval(() => {
+      this.className = 'purple';
+      setTimeout(() => {
+        this.className = '';
+        }, 3000);
+    }, 2000);
   }
-// }
+}
 
 window.customElements.define('smb-nose', SmbNose);
